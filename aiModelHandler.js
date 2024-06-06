63601d3e7c2fbc729cef2683f4f8d93dbd2d9280
Neerav Sood <165214507+NeeravSood@users.noreply.github.com>
2024-06-06 01:16:15 -0400

    Update aiModelHandler.js
    1.0

--- aiModelHandler.js
+++ aiModelHandler.js
@@ -1,1 +1,18 @@
+const { AutoTokenizer, AutoModelForCausalLM } = require('transformers');
+
+let tokenizer, model;
+
+const loadModel = async () => {
+    tokenizer = await AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf");
+    model = await AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf");
+    console.log("Model loaded successfully");
+};
+
+const handleMessage = async (text) => {
+    const inputs = tokenizer(text, return_tensors='pt');
+    const outputs = await model.generate(inputs);
+    return tokenizer.decode(outputs[0], skip_special_tokens=true);
+};
+
+module.exports = { loadModel, handleMessage };
 
